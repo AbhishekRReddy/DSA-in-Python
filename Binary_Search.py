5b09744c2aa7fc4ce06fb681382f5816c9dc3347
AbhishekRReddy <arreddy1997@gmail.com>
2023-01-28 20:21:13 +0530

    Checking for the Empty List

--- Binary_Search.py
+++ Binary_Search.py
@@ -1,21 +1,15 @@
 '''
-
-
+Binary Search
 '''
 
 def locate_card(cards,query):
     for pos,elem in enumerate(cards):
         if elem==query:
             return pos
+    return False
 
-
-
-
-
-output=3
-
-test_cases={'input':{'cards':[13,11,10,7,4,3,1,0],
-                     'query':7},
-            'output':3
+test_cases={'input':{'cards':[],
+                     'query':4},
+            'output':4
 }
-print(locate_card(**test_cases['input'])==output)
+print(locate_card(**test_cases['input'])==test_cases['output'])
