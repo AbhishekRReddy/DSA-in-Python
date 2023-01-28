ea66435a3e3fa713b7a1e24b4274b62ff3eb4162
AbhishekRReddy <arreddy1997@gmail.com>
2023-01-28 19:54:08 +0530

    First Commit DSA in python

---
+++ Binary_Search.py
@@ -0,0 +1,17 @@
+'''
+
+
+'''
+
+def locate_card(cards,query):
+    pass
+
+
+
+output=3
+
+test_cases={'input':{'cards':[13,11,10,7,4,3,1,0],
+                     'query':7},
+            'output':3
+}
+print(locate_card(**test_cases['input'])==output)
