4554d0dd798cd0f881635d9bc4941d2667e4ba07
AbhishekRReddy <arreddy1997@gmail.com>
2023-01-30 22:18:17 +0530

    Finding the Linked List Length by following methods 1. Iterative approach 2. Recursive approach 3. Tail Recursive approach

--- Linked_List_Udacity_Assigment.py
+++ Linked_List_Udacity_Assigment.py
@@ -88,20 +88,20 @@ ll.append(e3)
 
 # Test get_position
 # Should print 3
-print ll.head.next.next.value
+print (ll.head.next.next.value)
 # Should also print 3
-print ll.get_position(3).value
+print (ll.get_position(3).value)
 
 # Test insert
 ll.insert(e4,3)
 # Should print 4 now
-print ll.get_position(3).value
+print (ll.get_position(3).value)
 
 # Test delete
 ll.delete(1)
 # Should print 2 now
-print ll.get_position(1).value
+print (ll.get_position(1).value)
 # Should print 4 now
-print ll.get_position(2).value
+print (ll.get_position(2).value)
 # Should print 3 now
-print ll.get_position(3).value
+print (ll.get_position(3).value)
