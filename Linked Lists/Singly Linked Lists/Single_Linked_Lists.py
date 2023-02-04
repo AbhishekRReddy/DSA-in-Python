006ccc5bd8a46db03dbc46cf3bbb02df5049a2de
AbhishekRReddy <arreddy1997@gmail.com>
2023-02-04 17:33:48 +0530

    Implemented the insertion function which can isnert the elemenet at start,end and anywhere

--- Linked Lists/Singly Linked Lists/Single_Linked_Lists.py
+++ Linked Lists/Singly Linked Lists/Single_Linked_Lists.py
@@ -3,6 +3,41 @@ class LinkedLists:
         self.head=node
         self.tail=None
 
+    def __iter__(self):
+        node=self.head
+        while node:
+            yield node
+            node=node.next
+
+    def insertion(self,value,location):
+        new_node=Node(value)
+        if self.head is None:
+            self.head=new_node
+            self.tail=new_node
+            return
+        else:
+            if location==0:
+                new_node.next=self.head
+                self.head=new_node
+                return
+            elif location==-1:
+                new_node.next=None
+                self.tail.next=new_node
+                self.tail=new_node
+            else:
+                counter=0
+                pointer=self.head
+                while pointer.next:
+                    if(counter==location):
+                        new_node.next=pointer.next
+                        pointer.next=new_node
+                        return 
+                    counter+=1
+                    pointer=pointer.next
+
+        
+
+
 class Node:
     def __init__(self,value=None):
         self.value=value
@@ -11,8 +46,8 @@ class Node:
     
 
 ll=LinkedLists()
-node1=Node(1)
-node2=Node(2)
-ll.head=node1
-ll.head.next=node2
-ll.tail=node2
+ll.insertion(5,0)
+ll.insertion(6,0)
+
+ll.insertion(99,-1)
+print([node.value for node in ll])
