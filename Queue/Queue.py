666b9d008ffabb151a616931d3aa98b68cccddd3
AbhishekRReddy <arreddy1997@gmail.com>
2023-02-12 11:00:06 +0530

    Implemented isempty and enqueue method

--- Queue/Queue.py
+++ Queue/Queue.py
@@ -4,5 +4,14 @@ class Queue:
     
     def __str__(self):
         values = [str(value) for value in values]
-        return '-->'.join(values)
+        return '<--'.join(values)
+    
+    def isEmpty(self):
+        if self.queue == []:
+            return True
+        return False
+    
+    def enqueue(self,value):
+        self.queue.append(value)
+        return 'Successfully enqueued'
     
