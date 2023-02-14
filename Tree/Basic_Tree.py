9928b7778f296248e7c1ad52b61d2c555df98321
AbhishekRReddy <arreddy1997@gmail.com>
2023-02-14 20:08:31 +0530

    Tree terminologies

---
+++ Tree/Basic_Tree.py
@@ -0,0 +1,9 @@
+'''
+Root: Node with no parent
+Edge :Link between parent and child
+Leaf: No Child
+Ancestor: parent/grand parent/ great grand parent
+Depth : Length from the parent node 
+Height: length from the node to the leaf node
+
+'''
