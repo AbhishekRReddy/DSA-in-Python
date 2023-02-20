a45fc340e6f081abe18fa9b8c6497299057f5564
AbhishekRReddy <arreddy1997@gmail.com>
2023-02-20 21:49:33 +0530

    Implemented the insert node method insidethe binary tree class

--- Tree/BInary_Tree_List.py
+++ Tree/BInary_Tree_List.py
@@ -1,6 +1,6 @@
 class BinaryTree:
     def __init__(self, size):
-        self.list = size * [None]
+        self.list = (size) * [None]
         self.max_size = size
         self.last_used_index = 0
 
@@ -11,5 +11,17 @@ class BinaryTree:
             str = f'Binary Tree of size {self.max_size} exists with elements of {self.list}'
             return str
 
+    def insert_node(self, value):
+        if self.last_used_index + 1 == self.max_size:
+            print('Binary Tree is Full')
+            return
+        self.list[self.last_used_index+1] = value
+        self.last_used_index += 1
+
+
 btree = BinaryTree(10)
+for i in range(1,10):
+    btree.insert_node(i)
 print(btree)
+
+
