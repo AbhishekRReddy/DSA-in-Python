131044e8227214d257f25112c7e3765d633d5beb
AbhishekRReddy <arreddy1997@gmail.com>
2023-02-21 21:25:44 +0530

    Implemented the search method using level order traversal

--- Tree/BInary_Tree_List.py
+++ Tree/BInary_Tree_List.py
@@ -1,14 +1,14 @@
 class BinaryTree:
     def __init__(self, size):
-        self.list = (size) * [None]
-        self.max_size = size
+        self.list = (size+1) * [None]
+        self.max_size = size+1
         self.last_used_index = 0
 
     def __str__(self):
         if self.last_used_index == 0:
             return 'Binary Tree does not exists'
         else:
-            str = f'Binary Tree of size {self.max_size} exists with elements of {self.list}'
+            str = f'Binary Tree of size {self.max_size-1} exists with elements of {self.list}'
             return str
 
     def insert_node(self, value):
@@ -17,11 +17,21 @@ class BinaryTree:
             return
         self.list[self.last_used_index+1] = value
         self.last_used_index += 1
+    
+    def search_node(self, value):
+        for node_value in self.list:
+            if(value == node_value):
+                print('Value has been found')
+                return
+        print('Value does not found in BT')
+        return
+
 
 
 btree = BinaryTree(10)
-for i in range(1,10):
+for i in range(1,11):
     btree.insert_node(i)
 print(btree)
+btree.search_node(11)
 
 
