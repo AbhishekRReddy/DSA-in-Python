4815ae2e3dad80aed6fc962008c00353088e262c
AbhishekRReddy <arreddy1997@gmail.com>
2023-02-23 22:55:25 +0530

    Implmented the Inorder traversal for the BST

--- Tree/Binary_Search_Tree.py
+++ Tree/Binary_Search_Tree.py
@@ -1,10 +1,48 @@
 class BSTNode:
-    def __init__(self, value):
+    def __init__(self, value=None):
         self.data = value
         self.left_node = None
         self.right_node = None
 
 class BST:
     def __init__(self,value = None):
-        self.root = BSTNode(value)
+        self.root = None
+
+    def core_insert_node(self, node, value):
+        if value <= node.data:
+            if node.left_node == None:
+                node.left_node = BSTNode(value)
+            else:
+                self.core_insert_node(node.left_node , value)
+        else:
+            if node.right_node == None:
+                node.right_node = BSTNode(value)
+            else:
+                self.core_insert_node(node.right_node, value)
+        print('Node has been successfully inserted')
+    def insert_node(self, value):
+        if self.root is None:
+            self.root = BSTNode(value)
+            return
+        else:
+            self.core_insert_node(self.root, value)
+
+
+    def core_inorder_traverse(self, node):
+        if node is None:
+            return
+        self.core_inorder_traverse(node.left_node)
+        print(node.data)
+        self.core_inorder_traverse(node.right_node)
+    
+    def inorder_traverse(self):
+        self.core_inorder_traverse(self.root)
+
+
+bst = BST()
+
+for i in range(50,0,-1):
+    bst.insert_node(i)
+    
+bst.inorder_traverse()
 
