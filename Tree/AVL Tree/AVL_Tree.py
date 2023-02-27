9828c0ae918df3a404fd03b896c5cff0de9d3df0
AbhishekRReddy <arreddy1997@gmail.com>
2023-02-27 22:07:36 +0530

    Impemented the search method for AVL tree

--- Tree/AVL Tree/AVL_Tree.py
+++ Tree/AVL Tree/AVL_Tree.py
@@ -40,7 +40,21 @@ def level_order_traverse(node):
         if temp_node.value.right_child:
             queue.enqueue(temp_node.value.right_child)
 
+def search(node , value):
+    if node is None:
+        print('Element does not exists anywhere')
+        return
+    elif node.data == value:
+        print('Element does exists in the tree')
+        return
+    elif node.data > value:
+        search(node.left_child, value)
+    elif node.data < value:
+        search(node.right_child, value)
+    
 
+
+    
 
 
 
@@ -69,4 +83,4 @@ node30.right_child = node40
 node90.left_child = node80
 node90.right_child = node100
 
-level_order_traverse(root_node)
+search(root_node, 600)
