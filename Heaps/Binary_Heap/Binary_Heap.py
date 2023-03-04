3ba567f44fa862e534599c2440037c23b67106ff
AbhishekRReddy <arreddy1997@gmail.com>
2023-03-04 17:12:06 +0530

    Impemented the Peek_heap function which helps to return the root value of the heap

--- Heaps/Binary_Heap/Binary_Heap.py
+++ Heaps/Binary_Heap/Binary_Heap.py
@@ -4,4 +4,10 @@ class Heap:
         self.current_heap_size = 0
         self.max_heap_size = size + 1
 
+def peek_heap(root_node):
+    if root_node is None:
+        return None
+    return root_node.custom_list[1]
+
+
 heap = Heap(5)
