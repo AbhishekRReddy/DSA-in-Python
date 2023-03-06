c581dc82d1384d1fe18c925ae4d509541d033a8a
AbhishekRReddy <arreddy1997@gmail.com>
2023-03-06 21:35:54 +0530

    Multiple_Linear_Search with creating the local list returning

--- Recursion/Multiple_Linear_Search.py
+++ Recursion/Multiple_Linear_Search.py
@@ -1,3 +1,4 @@
+
 def recursive_search(numbers, value, index = 0, index_list=[]):
     if index == len(numbers):
         return index_list
@@ -5,6 +6,17 @@ def recursive_search(numbers, value, index = 0, index_list=[]):
         index_list.append(index)
     return recursive_search(numbers, value, index+1, index_list)
 
+#Using the list inside the function body
+def recursive_search_2(numbers, value, index =0):
+    a =[]
+    if index == len(numbers):
+        return a
+    if numbers[index] == value:
+        a.append(index)
+    return a + recursive_search_2(numbers, value, index+1)
+
+
 numbers = [10,20,30,40,50,60,70,80,20,30,20,40,20,20,]
 print(numbers)
-print(recursive_search(numbers, 20))
+list = recursive_search_2(numbers, 20)
+print(list)
