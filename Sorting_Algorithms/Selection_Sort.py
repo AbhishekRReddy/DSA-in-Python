7e0240dd18c48810724dc66c2ea47e3a60284a75
AbhishekRReddy <arreddy1997@gmail.com>
2023-03-09 19:28:44 +0530

    Swapping changed only once after finding the minimum element in the sub array

--- Sorting_Algorithms/Selection_Sort.py
+++ Sorting_Algorithms/Selection_Sort.py
@@ -1,10 +1,14 @@
 def selection_sort(numbers):
     num_len = len(numbers)
     for i in range(num_len):
+        min_index = i
         for j in range(i+1, num_len):
-            if numbers[j] < numbers[i]:
-                numbers[i],numbers[j] = numbers[j],numbers[i]
+            if numbers[j] < numbers[min_index]:
+                min_index = j
+        numbers[i], numbers[min_index] = numbers[min_index], numbers[i]                
+
+
 
 numbers = [4,1,2,6,7,3,7,11,13,0,1,2,3]
 selection_sort(numbers)
-print(numbers)
+print(numbers)  
