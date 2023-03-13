c6154b409d457e3cb25056ca3107cd82d3b256c4
AbhishekRReddy <arreddy1997@gmail.com>
2023-03-13 19:29:52 +0530

    Implemented the Quick_Sort

--- Sorting_Algorithms/Quick_Sort.py
+++ Sorting_Algorithms/Quick_Sort.py
@@ -1,6 +1,6 @@
 def swap(nums, index1, index2):
     nums[index1], nums[index2] = nums[index2], nums[index1]
-
+#Pivot Function allocates the number to its specified value
 def pivot_func(nums, pivot, last_index):
     swap_index = pivot
     for i in range(pivot+1,last_index+1):
@@ -10,7 +10,14 @@ def pivot_func(nums, pivot, last_index):
     swap(nums,pivot,swap_index)
     return swap_index
 
+def quick_sort(nums,left,right):
+    if left == right:
+        return
+    pivot_index = pivot_func(nums,left,right)
+    quick_sort(nums,left,pivot_index-1)
+    quick_sort(nums,pivot_index+1,right)
+
 nums = [3,5,0,6,2,1,4]
-print(pivot_func(nums,0,len(nums)-1))
+print(quick_sort(nums, 0 ,len(nums)-1))
 print(nums)
 
