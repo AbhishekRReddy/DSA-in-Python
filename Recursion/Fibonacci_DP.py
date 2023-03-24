d4f3e5d7c9d7b13d099e5b12f7c0837bd834e286
AbhishekRReddy <arreddy1997@gmail.com>
2023-03-24 18:39:05 +0530

    Dynamic programming approach to the print n fibonacci numbers

---
+++ Recursion/Fibonacci_DP.py
@@ -0,0 +1,19 @@
+def fibo(n):
+    if n == 0:
+        print(0)
+        return
+    if n == 1:
+        print(1)
+        return 
+    fibo_val1 = 0
+    fibo_val2 = 1
+    print(fibo_val1,end=' ')
+    print(fibo_val2,end =' ')
+    for i in range(2,n+1):
+        temp = fibo_val1
+        fibo_val1 = fibo_val2
+        fibo_val2 = temp + fibo_val1
+        print(fibo_val2,end= ' ')
+
+fibo(150)
+
