a85ca1b75fd6de29e84cec978f0525f4060a2a8c
AbhishekRReddy <arreddy1997@gmail.com>
2023-03-24 10:01:14 +0530

    Implemented the Edge and Node class which helps in Dijkstra Algorithm

--- Graph/dijkstras_algorithms.py
+++ Graph/dijkstras_algorithms.py
@@ -1,3 +1,20 @@
+class Edge:
+    def __init__(self,start_vertex,end_vertex,weight):
+        self.start_vertex = start_vertex
+        self.end_vertex = end_vertex
+        self.weight = weight
+
+class Node:
+    def __init__(self,name):
+        self.name = name
+        self.visited = False
+        self.previous_node = None
+        self.neighbours = []
+        self.min_distance = float('inf')
+    
+    def __lt__(self,other_node):
+        return self.min_distance < other_node.min_distance
+
 class Graph:
     def __init__(self):
         self.adj_list = {}
