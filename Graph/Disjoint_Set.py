1f085177d74ccdc9d06f134a839e9448ad34b02d
AbhishekRReddy <arreddy1997@gmail.com>
2023-03-26 19:12:39 +0530

    Implemented the Disjoint_Set class which supports the MST algorithms

---
+++ Graph/Disjoint_Set.py
@@ -0,0 +1,29 @@
+class DisjointSet:
+    def __init__(self, vertices):
+        self.vertices = vertices
+        self.parent = {}
+        for v in vertices:
+            #Initializing the parent of all the vertices to itself, in the beginning.
+            self.parent[v] = v
+        self.rank = dict.fromkeys(vertices, 0)
+    
+    def find_item(self, item):
+        if self.parent[item] == item:
+            return item
+        else:
+            self.find_item(self.parent[item])
+    
+    def union(self,x, y):
+        xroot = self.find_item(x)
+        yroot = self.find_item(y)
+        if self.rank[xroot] < self.rank[yroot]:
+            self.parent[xroot] = yroot
+        elif self.rank[xroot] > self.rank[yroot]:
+            self.parent[yroot] = xroot
+        else:
+            self.parent[yroot] = xroot
+            self.rank[xroot] +=1
+
+vertices = ['A','B','C','D','E']
+ds = DisjointSet(vertices)
+print(ds.find_item('A'))
