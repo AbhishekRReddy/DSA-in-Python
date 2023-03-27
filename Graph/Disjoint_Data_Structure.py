8cf1d3febd36c535266095801c85e0ac996f3b3d
AbhishekRReddy <arreddy1997@gmail.com>
2023-03-27 21:45:54 +0530

    Implemented the Disjoint_Data_Structure where union is done by size where it contains the count of all the elements in the graph

--- Graph/Disjoint_Data_Structure.py
+++ Graph/Disjoint_Data_Structure.py
@@ -5,6 +5,7 @@ class Disjoint:
     def __init__(self,no_vertices):
         self.parent = []
         self.rank = [0] * (no_vertices+1)
+        self.size = [1] * (no_vertices+1)
         for i in range(no_vertices+1):
             self.parent.append(i)
     
@@ -16,10 +17,10 @@ class Disjoint:
         in the constant time'''
         return self.parent[node]
     
-    def union(self,u, v):
+    def union_by_rank(self,u, v):
         ult_u = self.find_ult_parent(u)
         ult_v = self.find_ult_parent(v)
-        if ult_u == ult_v:
+        if ult_u == ult_v: #If both parents are same means they are in same set.
             return
         elif self.rank[ult_u] > self.rank[ult_v]:
             self.parent[ult_v] = ult_u
@@ -34,13 +35,23 @@ class Disjoint:
         taken to compute the parent during the path compression will be 
         less.
         '''
-
+    def union_by_size(self,u,v):
+        ult_u = self.find_ult_parent(u)
+        ult_v = self.find_ult_parent(v)
+        if ult_u == ult_v:
+            return
+        elif self.size[ult_u] < self.size[ult_v]:
+            self.parent[ult_u] = ult_v
+            self.size[ult_v] += self.size[ult_u]
+        else:
+            self.parent[ult_v] = ult_u
+            self.size[ult_u] += self.size[ult_v]
 ds = Disjoint(7)
-ds.union(1,2)
-ds.union(3,4)
-ds.union(4,5)
-ds.union(5,6)
-ds.union(6,7)
+ds.union_by_size(1,2)
+ds.union_by_size(3,4)
+ds.union_by_size(4,5)
+ds.union_by_size(5,6)
+ds.union_by_size(6,7)
 print(ds.find_ult_parent(7))        
 
 
