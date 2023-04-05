bd587e6fb815c261eb199bf5788612fd6f98fdcf
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-05 22:13:55 +0530

    Fibonacci_Top_Down approach using Dynamic_Programming

---
+++ Dynamic_Programming/Fibonacci_Top_Down.py
@@ -0,0 +1,11 @@
+def fibo(n, memo):
+    if n == 1:
+        return 0
+    if n == 2:
+        return 1
+    if not n in memo:
+        memo[n] = fibo(n-1,memo) + fibo(n-2,memo)
+    return memo[n]
+
+dict ={}
+print(fibo(5,dict))
