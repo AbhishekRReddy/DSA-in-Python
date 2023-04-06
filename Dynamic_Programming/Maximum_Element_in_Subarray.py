aadbda16ecd8cb6c36418579a973abaad36148df
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-06 23:14:04 +0530

    Implemented the Maximum_Element_in_Subarray with two traversal approach where it takes O(N) TC but we are traversing twice separately.

--- Dynamic_Programming/Maximum_Element_in_Subarray.py
+++ Dynamic_Programming/Maximum_Element_in_Subarray.py
@@ -11,4 +11,39 @@ def max_product(nums):
             global_max = max(global_max, local_max)
     return global_max
 
-print(max_product([1,2,3,4,5,0]))
+
+#Two traversals method
+'''
+If zero is countered we are resetting the subarray. Suppose if one zero is present inside the array
+then there will two subarrays on either side of the zero
+[-2,-1,3,0,-4]
+At the end we are comparaing the max_left and max_right with zero also because zero is also one subarray
+and two subarrays product can be negatives
+'''
+
+def max_product_two_traverse(nums):
+    max_left = nums[0]
+    max_right = nums[0] #Choose any of the element in the given array
+    is_zero_encounterd = False
+    prod = 1
+    for n in nums:
+        prod *= n
+        if n == 0:
+            is_zero_encounterd = True #Refer to above why we are using the is_zero_encoutnered
+            prod = 1
+            continue
+        max_left = max(max_left, prod)
+    prod = 1
+    for n in reversed(nums):
+        prod *= n
+        if n == 0:
+            is_zero_encounterd = True
+            prod = 1
+            continue
+        max_right = max(max_right, prod)
+    if is_zero_encounterd:
+        return max(max(max_left,max_right), 0) #Considering the subarrau of the zero
+    return max(max_right,max_left)
+
+
+print(max_product_two_traverse([-2,-1,-3,0,-4]))
