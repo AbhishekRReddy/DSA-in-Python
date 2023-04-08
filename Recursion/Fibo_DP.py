83fd0b17934c247c3edcfd1be365ddd98c1d1956
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-08 14:59:55 +0530

    With space optimization

--- Recursion/Fibo_DP.py
+++ Recursion/Fibo_DP.py
@@ -14,9 +14,17 @@ def fibo_tab(n,dp):
         dp[i] = dp[i-1] + dp[i-2]
     return dp[n]
 
+def febo_space(n):
+    prev1 = 0
+    prev2 = 1
+    for i in range(2,n+1):
+        curr = prev1 + prev2
+        prev2 = prev1
+        prev1 = curr
+    return curr
+
 
 
 n = 7
 dp = [-1]*(n+1)
-print(fibo_tab(n,dp))
-print(dp)
+print(febo_space(7))
