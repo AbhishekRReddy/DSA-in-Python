dae39fa0c954cdb76f08b1b7668ce21d1609beea
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-08 11:43:30 +0530

    Commented for clarirty

--- Recursion/Subsequence.py
+++ Recursion/Subsequence.py
@@ -1,16 +1,15 @@
 '''
-TC will be 2^n where n is number of elements in given array
+TC will be 2^N where n is number of elements in given array
 For every element we will be having two choices, that is to take it or not take it.
 So 2^N 
 '''
-
 def subsequence(index, sub_seq,nums):
     if index == len(nums):
         print(sub_seq)
         return
-    sub_seq.append(nums[index])
+    sub_seq.append(nums[index]) #Take Condition
     subsequence(index+1, sub_seq, nums)
-    sub_seq.remove(nums[index])
+    sub_seq.remove(nums[index]) #Dont't take condition
     subsequence(index+1, sub_seq, nums)
 
 nums= [3,1,2]
