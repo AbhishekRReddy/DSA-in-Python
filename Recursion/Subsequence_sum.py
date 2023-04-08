5843e9ebaf410d0c723a18d5c2b81df64ab1aa87
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-08 12:32:00 +0530

    Only one Subsequence with sum equals to total.

--- Recursion/Subsequence_sum.py
+++ Recursion/Subsequence_sum.py
@@ -2,11 +2,14 @@ def sub_seq_sum(i,sub,nums,total):
     if i == len(nums):
         if sum(sub) == total:
             print(sub)
-            return
-        return
+            return True #Prints only one subsequence    
+        return False
     sub.append(nums[i])
-    sub_seq_sum(i+1,sub,nums,total)
+    if (sub_seq_sum(i+1,sub,nums,total) is True):
+        return True #We need return True whoever call.
     sub.pop()
-    sub_seq_sum(i+1,sub,nums,total)
+    if (sub_seq_sum(i+1,sub,nums,total) is True):
+        return True
+    return False #None of the functions retuned true
 
 sub_seq_sum(0,[],[1,2,1],2)
