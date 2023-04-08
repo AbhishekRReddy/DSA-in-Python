152ddde4dc14a6e6682d48eb392d406c9a838a35
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-08 14:52:21 +0530

    Tabular method of the fibonacci series dp

--- Recursion/Fibo_DP.py
+++ Recursion/Fibo_DP.py
@@ -7,7 +7,16 @@ def fib(n,dp):
     dp[n] = fib(n-1,dp) + fib(n-2, dp)
     return dp[n]
 
-n = 2
+def fibo_tab(n,dp):
+    dp[0] = 0
+    dp[1] = 1
+    for i in range(2,n+1):
+        dp[i] = dp[i-1] + dp[i-2]
+    return dp[n]
+
+
+
+n = 7
 dp = [-1]*(n+1)
-print(fib(n,dp))
+print(fibo_tab(n,dp))
 print(dp)
