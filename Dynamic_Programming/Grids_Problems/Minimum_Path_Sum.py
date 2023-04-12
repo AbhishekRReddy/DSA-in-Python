5a1875db55dc37177ec2f083e494c7854ed2f6a1
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-12 19:58:16 +0530

    Minimum_Path_Sum with DP tabulation

--- Dynamic_Programming/Grids_Problems/Minimum_Path_Sum.py
+++ Dynamic_Programming/Grids_Problems/Minimum_Path_Sum.py
@@ -23,3 +23,25 @@ class Solution:
         n = len(grid[0])
         dp = [[-1]*n for _ in range(m)]
         return minips(m-1,n-1,grid,dp)
+
+'''
+DP with Tabulation
+'''
+class Solution:
+    def minPathSum(self, grid: list[list[int]]) -> int:
+        m = len(grid)
+        n = len(grid[0])
+        dp = [[-1]*n for _ in range(m)]
+        for i in range(m):
+            for j in range(n):
+                up = float('inf')
+                left = float('inf')
+                if i == 0 and j == 0:
+                    dp[0][0] = grid[0][0]
+                    continue
+                if i > 0:
+                    up = grid[i][j] + dp[i-1][j]
+                if j > 0:
+                    left = grid[i][j] + dp[i][j-1]
+                dp[i][j] = min(up,left)
+        return dp[m-1][n-1]
