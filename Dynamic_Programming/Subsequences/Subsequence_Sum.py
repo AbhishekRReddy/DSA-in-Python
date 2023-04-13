2ed3c0091d79e9f339460c453e3c36efcab7d968
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-13 19:37:25 +0530

    Subsequence_Sum with DP memoization

--- Dynamic_Programming/Subsequences/Subsequence_Sum.py
+++ Dynamic_Programming/Subsequences/Subsequence_Sum.py
@@ -13,3 +13,27 @@ def subek(n,k,arr):
     if k >= arr[n]:
         take = subek(n-1,k-arr[n],arr)
     return take or not_take
+
+'''
+DP with Memoization
+'''
+def subek(n,k,arr,dp):
+    if k == 0:
+        return True
+    if n == 0:
+        return k == arr[0]
+    if dp[n][k] != -1:
+        return dp[n][k]
+    not_take = subek(n-1,k,arr,dp)
+    take = False
+    if k >= arr[n]:
+        take = subek(n-1,k-arr[n],arr,dp)
+    dp[n][k] = take or not_take
+    return dp[n][k]
+
+def subsetSumToK(n, k, arr):
+    dp = [ [-1]*(k+1) for _ in range(n)]
+    # Write your code here
+    # Return a boolean variable 'True' or 'False' denoting the answer
+    return subek(n-1,k,arr,dp)
+    
