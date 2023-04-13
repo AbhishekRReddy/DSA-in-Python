333afc33482e2a8027d91256ba6c69cefd818f03
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-13 22:09:46 +0530

    0_1_Knapsack by Memoization

--- Dynamic_Programming/Subsequences/0_1_Knapsack.py
+++ Dynamic_Programming/Subsequences/0_1_Knapsack.py
@@ -15,3 +15,25 @@ def knapsk(values,weights,n,w):
         pick =values[n] + knapsk(values,weights,n-1,w-weights[n])
     return max(pick,not_pick)
 
+'''
+DP with Memoization
+'''
+def knapsk(values,weights,n,w,dp):
+    if n == 0:
+        if w >= weights[n]:
+            return values[n]
+        return 0
+    if dp[n][w] != -1:
+        return dp[n][w]
+    not_pick = knapsk(values,weights,n-1,w,dp)
+    pick = float('-inf')
+    if w >= weights[n]:
+        pick =values[n] + knapsk(values,weights,n-1,w-weights[n],dp)
+    dp[n][w] = max(pick,not_pick)
+    return dp[n][w]
+
+def maxProfit(values, weights, n, w):
+    dp = [ [-1]*(w+1) for i in range(n)] 
+    return knapsk(values,weights,n-1,w,dp)
+
+
