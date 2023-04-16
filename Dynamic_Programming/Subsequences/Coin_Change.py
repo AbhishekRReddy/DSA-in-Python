ece246d99b3ad667de8690a688ba0db7fdb0f706
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-16 21:48:44 +0530

    modified

--- Dynamic_Programming/Subsequences/Coin_Change.py
+++ Dynamic_Programming/Subsequences/Coin_Change.py
@@ -4,12 +4,12 @@ https://www.codingninjas.com/codestudio/problems/ways-to-make-coin-change_630471
 def way(d,value, index):
     if value == 0:
         return 1 
-    if value < 0:
+    if value < 0 or index < 0:
         return 0
-    pick = way(d,value-d[index],index)
-    not_pick = 0
-    if index > 0:
-        not_pick = way(d,value,index-1)
+    pick = 0
+    if d[index]<=value:
+        pick = way(d,value-d[index],index)
+    not_pick = way(d,value,index-1)
     return pick + not_pick
 
 
