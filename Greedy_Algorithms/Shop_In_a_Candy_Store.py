5b8e20454e47a5ef298f22d3cdc32cf92c03ea61
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-18 22:30:16 +0530

    Shop_In_a_Candy_Store with Greedy_Approach

---
+++ Greedy_Algorithms/Shop_In_a_Candy_Store.py
@@ -0,0 +1,23 @@
+'''
+https://practice.geeksforgeeks.org/problems/shop-in-candy-store1145/1?utm_source=geeksforgeeks&utm_medium=article_practice_tab&utm_campaign=article_practice_tab
+'''
+
+class Solution:
+
+    def candyStore(self, candies,N,K):
+        # code here
+        candies.sort()
+        for_max = candies[:]
+        min = 0
+        max = 0
+        while len(for_max)>0:
+            min += for_max.pop(0)
+            for _ in range(K):
+                if len(for_max)>0:
+                    for_max.pop()
+        while len(candies)>0:
+            max += candies.pop()
+            for _ in range(K):
+                if len(candies)>0:
+                    candies.pop(0)
+        return min,max
