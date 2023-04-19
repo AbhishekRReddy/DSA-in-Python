5097dafbad389b5b50ba05d55330371c8b1a6bbd
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-19 22:22:24 +0530

    Eliminated unnecessary variables

--- Greedy_Algorithms/Rope.py
+++ Greedy_Algorithms/Rope.py
@@ -6,16 +6,14 @@ import heapq
 class Solution:
     #Function to return the minimum cost of connecting the ropes.
     def minCost(self,arr,n) :
-        if n == 1:
-            return 0
         heapq.heapify(arr)
         cost = 0
         global_cost = 0
-        while len(arr) > 2:
+        while len(arr) > 1:
             a = heapq.heappop(arr)
             b = heapq.heappop(arr)
             cost = a + b
             global_cost += cost
             heapq.heappush(arr,cost)
-        return arr[0] + arr[1] + global_cost
+        return global_cost
 
