f39a39d10bf07905d3d49f85fcfb6d7a4a503458
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-20 22:17:29 +0530

    DP with tabulation

--- Dynamic_Programming/Fibo_Repeat.py
+++ Dynamic_Programming/Fibo_Repeat.py
@@ -20,3 +20,20 @@ class Solution:
     def fib(self, n: int) -> int:
         dp = [-1]*(n+1)
         return fibo(n,dp)
+
+'''
+DP with tabulation
+'''
+
+class Solution:
+    def fib(self, n: int) -> int:
+        dp = [-1]*(n+1)
+        dp[0] = 0
+        if n == 0:
+            return dp[0]
+        dp[1] = 1
+        if n == 1:
+            return dp[1]
+        for i in range(2,n+1):
+            dp[i] = dp[i-1] + dp[i-2]
+        return dp[n]
