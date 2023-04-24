f1073e6bf32296e4fd39728cff64afed1b06724a
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-24 22:38:22 +0530

    DP with Tabualation - array is left shifted to accomodate the base cases.

--- Dynamic_Programming/DP_On_Subsequence/Longest_Common_Subsequence.py
+++ Dynamic_Programming/DP_On_Subsequence/Longest_Common_Subsequence.py
@@ -34,3 +34,26 @@ class Solution:
         n= len(text2)
         dp = [ [-1]*n for _ in range(m) ]
         return lstr(text1,text2,m-1,n-1,dp)
+
+'''
+DP with Tabualation - array is left shifted to accomodate the base cases.
+'''
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m = len(text1) + 1
+        n= len(text2) + 1 #For shifitng purpose
+        dp = [ [-1]*n for _ in range(m) ]
+        for x in range(n):
+            dp[0][x] = 0
+        for y in range(m):
+            dp[y][0] = 0
+
+        for x in range(1,m):
+            for y in range(1,n):
+                if text1[x-1] == text2[y-1]:
+                    dp[x][y] = 1 + dp[x-1][y-1]
+                    continue
+                non_match1 = dp[x][y-1]
+                non_match2 = dp[x-1][y]
+                dp[x][y] = max(non_match1, non_match2) 
+        return dp[m-1][n-1]
