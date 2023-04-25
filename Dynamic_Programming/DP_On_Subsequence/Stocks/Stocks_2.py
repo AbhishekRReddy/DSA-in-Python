5cbe428554dc84e922c2df599d5e16ba5e12ddf3
AbhishekRReddy <arreddy1997@gmail.com>
2023-04-25 22:19:21 +0530

    Memoization

--- Dynamic_Programming/DP_On_Subsequence/Stocks/Stocks_2.py
+++ Dynamic_Programming/DP_On_Subsequence/Stocks/Stocks_2.py
@@ -18,3 +18,28 @@ def f(ind,buy,prices):
 class Solution:
     def maxProfit(self, prices:list[int]) -> int:
         return f(0,True,prices)
+
+'''
+DP with Memoization
+'''
+def f(ind,buy,prices,dp):
+    if ind == len(prices):
+        return 0
+    profit = 0
+    if dp[ind][buy] != -1:
+        return dp[ind][buy]
+    if buy == 1:
+        pur = -prices[ind] + f(ind+1,0,prices,dp)
+        not_pur = f(ind+1, 1, prices,dp)
+        profit = max(pur,not_pur) 
+    else:
+        sell = prices[ind] + f(ind+1, 1, prices,dp)
+        not_sell = f(ind+1,0,prices,dp)
+        profit = max(sell,not_sell)
+    dp[ind][buy] = profit
+    return dp[ind][buy]
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:
+        dp = [ [-1,-1] for _ in range(len(prices)+1)]
+        return f(0,1,prices,dp)
+
