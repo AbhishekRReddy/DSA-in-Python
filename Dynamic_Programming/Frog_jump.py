a0959e5eb386b1d3cf6964eb924810aa627d593c
AbhishekRReddy <arreddy1997@gmail.com>
2023-06-08 07:09:49 +0530

    Memoization

--- Dynamic_Programming/Frog_jump.py
+++ Dynamic_Programming/Frog_jump.py
@@ -1,32 +1,27 @@
-'''
-https://www.codingninjas.com/codestudio/problems/frog-jump_3621012
-'''
-def frogJump(n: int, heights: list[int],dp) -> int:
-    # Write your code here.
-    if n == 1:
-        dp[n] = 0
-        return 0
-    if n in dp:
+from os import *
+from sys import *
+from collections import *
+from math import *
+from typing import *
+
+
+def jump(n,h,dp):
+    if n == 0:
+        dp[0] = 0
+        return 0      
+    if dp[n] != -1:
         return dp[n]
-    left = frogJump(n-1,heights) + abs(heights[n-1]-heights[n-2])
-    if n > 2:
-        right = frogJump(n-2,heights) + abs(heights[n-1]-heights[n-3])
-        dp[n] = min(left, right)
-        return dp[n]
-    dp[n] = left
+    take = jump(n-1,h,dp) + abs(h[n] - h[n-1])
+    not_take = float('inf')
+    if n > 1:
+        not_take = jump(n-2,h,dp) + abs(h[n] - h[n-2])
+    dp[n] = min(take, not_take)
     return dp[n]
 
-def frogJump_tab(n,heights):
-    prev1 = 0
-    prev2 = 0
-    for i in range(1, n):
-        one_jump = prev1 + abs(heights[i] - heights[i-1])
-        two_jump = float('inf')
-        if i>1:
-            two_jump = prev2 + abs(heights[i]- heights[i-2])
-        curri = min(one_jump,two_jump)
-        prev2 = prev1
-        prev1 = curri
-    return curri
-
-print(frogJump_tab(4,[10,20,30,10]))
+    
+def frogJump(n: int, heights: list[int]) -> int:
+    dp = [-1] * (n)
+    jump(n-1,heights,dp)
+    return dp[n-1]
+    
+    
